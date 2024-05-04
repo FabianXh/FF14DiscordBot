d1fd8757b9105d9a9eebfa790682014700081a1e
Matthew Nock <thematt.nock@gmail.com>
2024-05-04 11:55:18 +0100

    chore: Update discord.js import to version 14.14.1 and add ActivityFlagsBitField

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, Collection, Events, GatewayIntentBits, ActivityFlagsBitField } = require('discord.js');
+const { Client, Collection, GatewayIntentBits, ActivityFlagsBitField } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 const { token } = require('./config.json');
@@ -26,35 +26,18 @@ for (const folder of commandFolders) {
 		}
 	}
 }
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
 	}
+}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-// Log in to Discord with your client's token
 client.login(token);
