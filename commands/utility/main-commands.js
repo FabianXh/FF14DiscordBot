9ade214844ba85dd2b83447df124682a5aad0bf9
Matthew Nock <thematt.nock@gmail.com>
2024-05-04 11:06:41 +0100

    chore: Add new slash commands for user and server information

--- commands/utility/main-commands.js
+++ commands/utility/main-commands.js
@@ -1,3 +1,9 @@
 new SlashCommandBuilder()
 	.setName('ping')
 	.setDescription('Replies with Pong!');
+new SlashCommandBuilder()
+	.setName('user')
+	.setDescription('Provides information about the user.');
+new SlashCommandBuilder()
+	.setName('server')
+	.setDescription('Provides information about the server.');
