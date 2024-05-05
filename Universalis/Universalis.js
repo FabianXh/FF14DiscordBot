73508e8ea8c6160d14cc53492543da44440a5849
Matthew Nock <thematt.nock@gmail.com>
2024-05-05 09:26:10 +0100

    input itemID return cheapest item

--- Universalis/Universalis.js
+++ Universalis/Universalis.js
@@ -1,12 +1,12 @@
 const cheerio = require("cheerio")
 const axios = require("axios")
 
-async function performScraping() {
+async function performScraping(lookup) {
     // downloading the target web page
     // by performing an HTTP GET request in Axios
     const axiosResponse = await axios.request({
         method: "GET",
-        url: "https://universalis.app/market/39630",
+        url: `https://universalis.app/market/${lookup}`,
         headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
         }
