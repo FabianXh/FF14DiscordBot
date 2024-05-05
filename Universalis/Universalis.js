0056ab840dd60390c7799aec5fa826c530dc9320
Matthew Nock <thematt.nock@gmail.com>
2024-05-05 11:03:00 +0100

    fuck lodash

--- Universalis/Universalis.js
+++ Universalis/Universalis.js
@@ -16,12 +16,12 @@ async function performScraping(itemName) {
         `https://universalis.app/api/v2/light/39630?listings=5`
     );
     const lightData = lightResponse.data;
-    console.log(lightData);
+    //    console.log(lightData);
     const chaosResponse = await axios.get(
         `https://universalis.app/api/v2/light/39630?listings=5`
     );
     const chaosData = chaosResponse.data;
-    console.log(chaosData);
+    console.log(chaosData.listings[0].pricePerUnit);
 }
 // can delete when we load this into the bot
 performScraping("Diadochos Sword");
