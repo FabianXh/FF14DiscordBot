376c7b79b092a50172854a030c06d254fe27b847
Matthew Nock <thematt.nock@gmail.com>
2024-05-05 11:00:24 +0100

    universalis working

--- Universalis/Universalis.js
+++ Universalis/Universalis.js
@@ -1,26 +1,29 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-
-async function performScraping(lookup) {
+const items = require("./items.json");
+const { get } = require("axios");
+async function performScraping(itemName) {
     // downloading the target web page
     // by performing an HTTP GET request in Axios
-    const axiosResponse = await axios.request({
-        method: "GET",
-        url: `https://universalis.app/market/${lookup}`,
-        headers: {
-            "User-Agent":
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-        },
-    });
-    // using cheetio to format the web page into HTML
-    const $ = cheerio.load(axiosResponse.data);
-
-    // keeping this veriable for later
-    var cheapest = $(".cheapest").find("div").first().text();
-    // delete whenever you want this was for testing
-    console.log($(".cheapest").find("div").first().text());
-    return cheapest;
+    const itemId = Object.keys(items).find(
+        (key) => items[key]["en"] === itemName
+    );
+    if (!itemId) {
+        console.log("Item not found.");
+        return;
+    }
+    const lightResponse = await axios.get(
+        `https://universalis.app/api/v2/light/39630?listings=5`
+    );
+    const lightData = lightResponse.data;
+    console.log(lightData);
+    const chaosResponse = await axios.get(
+        `https://universalis.app/api/v2/light/39630?listings=5`
+    );
+    const chaosData = chaosResponse.data;
+    console.log(chaosData);
 }
 // can delete when we load this into the bot
-var cheapest = performScraping(39630);
+performScraping("Diadochos Sword");
+
 exports.performScraping = performScraping;
