100b2930a11e0980f2b445d0f1468542e7d19925
Matthew Nock <thematt.nock@gmail.com>
2024-05-06 21:56:32 +0100

    comments

--- Universalis/Universalis.js
+++ Universalis/Universalis.js
@@ -2,6 +2,7 @@ const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 const items = require('./items.json');
 
+// Function to perform scraping
 async function performScraping(itemName) {
     // Find the item ID from the items.json file
     const itemId = Object.keys(items).find(
@@ -20,6 +21,7 @@ async function performScraping(itemName) {
     const lightData = lightResponse?.data;
     const chaosData = chaosResponse?.data;
 
+    // Format the data for light listings
     const lightPrintData = lightData.listings
         .map((listing) => {
             const hq = listing.hq ? 'HQ' : '';
@@ -27,6 +29,7 @@ async function performScraping(itemName) {
         })
         .join('');
 
+    // Format the data for chaos listings
     const chaosPrintData = chaosData.listings
         .map((listing) => {
             const hq = listing.hq ? 'HQ' : '';
@@ -34,7 +37,8 @@ async function performScraping(itemName) {
         })
         .join('');
 
-    const embededMassage = new EmbedBuilder()
+    // Create an embedded message
+    const embeddedMessage = new EmbedBuilder()
         .setColor('89CFF0')
         .setTitle(itemName + ' Market Data')
         .setURL(`https://universalis.app/market/${itemId}`)
@@ -59,8 +63,9 @@ async function performScraping(itemName) {
                 'https://cdn.discordapp.com/attachments/1236243830886371330/1237138036777553940/204558a44722029ecc0dec40ec79c74e.jpeg?ex=663a8e14&is=66393c94&hm=09b0b70257cadc297a94980c98cd22e620f54a086a6f7b8584913e04895c68ae&',
         });
 
-    // return the data
-    return embededMassage;
+    // Return the embedded message
+    return embeddedMessage;
 }
 
+// Export the performScraping function
 exports.performScraping = performScraping;
