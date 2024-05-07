bc7c19548c303c009257db1c8a29834433e6f984
Matthew Nock <thematt.nock@gmail.com>
2024-05-07 21:40:16 +0100

    start of DR embed

--- commands/Utility/DR.js
+++ commands/Utility/DR.js
@@ -1,8 +1,9 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('death_roll') // Changed to meet Discord's requirements
@@ -14,7 +15,15 @@ module.exports = {
                 .setRequired(true)
         ),
     async execute(interaction) {
+        await interaction.deferReply();
         let input = interaction.options.getInteger('number');
-        await interaction.reply(getRandomInt(input).toString());
+        const embededMessageDR = new EmbedBuilder()
+            .setTitle('DEATH ROLL!')
+            .setDescription('Roll a number between 1 and the number you input')
+            .addFields({
+                name: 'ROLL',
+                value: getRandomInt(input).toString(),
+            });
+        await interaction.editReply({ embeds: [embededMessageDR] });
     },
 };
