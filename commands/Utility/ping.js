76a245f171b21e81a53bd617068d7cafab9314da
Matthew Nock <thematt.nock@gmail.com>
2024-05-08 23:29:14 +0100

    <se.6>

--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
-  async execute(interaction) {
-    await interaction.reply("Pong!");
-  },
+    data: new SlashCommandBuilder()
+        .setName('explain')
+        .setDescription('when someone starts to explain something'),
+    async execute(interaction) {
+        await interaction.reply('Wenz Pull! <se.6>');
+    },
 };
