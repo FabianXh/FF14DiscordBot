84dfedb0c28d67862ebe717c6b97e2102518a73c
Matthew Nock <thematt.nock@gmail.com>
2024-05-08 19:55:06 +0100

    its broke and i can't fix it

--- commands/Utility/DR.js
+++ commands/Utility/DR.js
@@ -1,69 +1,72 @@
 const {
-  Client,
-  GatewayIntentBits,
-  ButtonStyle,
-  SlashCommandBuilder,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-} = require("discord.js");
-const { token } = require("../../config.json");
+    Client,
+    GatewayIntentBits,
+    ButtonStyle,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+} = require('discord.js');
+const { token } = require('../../config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const row = new ActionRowBuilder();
 const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
 
 row.addComponents(
-  new ButtonBuilder()
-    .setCustomId("rollMore")
-    .setLabel("Roll")
-    .setStyle(ButtonStyle.Danger)
+    new ButtonBuilder()
+        .setCustomId('rollMore')
+        .setLabel('Roll')
+        .setStyle(ButtonStyle.Danger)
 );
 
 const createEmbedMessage = (input, roll) =>
-  new EmbedBuilder()
-    .setColor("DC143C")
-    .setTitle("DEATH ROLL!")
-    .setDescription(`Rolling a number between 1 and ${input}...`)
-    .addFields({ name: "RESULT", value: roll.toString() });
+    new EmbedBuilder()
+        .setColor('DC143C')
+        .setTitle('DEATH ROLL!')
+        .setDescription(`Rolling a number between 1 and ${input}...`)
+        .addFields({ name: 'RESULT', value: roll.toString() });
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("death_roll")
-    .setDescription("DEATH ROLL!")
-    .addIntegerOption((option) =>
-      option
-        .setName("number")
-        .setDescription("Input the number to roll")
-        .setRequired(true)
-    ),
+    data: new SlashCommandBuilder()
+        .setName('death_roll')
+        .setDescription('DEATH ROLL!')
+        .addIntegerOption((option) =>
+            option
+                .setName('number')
+                .setDescription('Input the number to roll')
+                .setRequired(true)
+        ),
 
-  async execute(interaction) {
-    await interaction.deferReply();
-    // const target = interaction?.options?.getUser("target");
-    const input = interaction.options.getInteger("number");
-    let roll = getRandomInt(input);
-    client.on("interactionCreate", (interaction) => {
-      if (!interaction.isButton() || interaction.customId !== "rollMore") {
-        return;
-      }
+    async execute(interaction) {
+        await interaction.deferReply();
+        // const target = interaction?.options?.getUser("target");
+        const input = interaction.options.getInteger('number');
+        let roll = getRandomInt(input);
+        client.on('interactionCreate', (interaction) => {
+            if (
+                !interaction.isButton() ||
+                interaction.customId !== 'rollMore'
+            ) {
+                return;
+            }
 
-      let oldMax = roll;
-      roll = getRandomInt(oldMax);
+            let oldMax = roll;
+            roll = getRandomInt(oldMax);
 
-      interaction.reply({
-        // content: `${target}`, Fuck you tygan
-        embeds: [createEmbedMessage(oldMax, roll)],
-        components: [row],
-      });
-    });
+            interaction.reply({
+                // content: `${target}`, Fuck you tygan
+                embeds: [createEmbedMessage(oldMax, roll)],
+                components: [row],
+            });
+        });
 
-    await interaction.editReply({
-      //   content: `${target}`,
-      embeds: [createEmbedMessage(input, roll)],
-      components: [row],
-    });
-  },
+        await interaction.editReply({
+            //   content: `${target}`,
+            embeds: [createEmbedMessage(input, roll)],
+            components: [row],
+        });
+    },
 };
 
 client.login(token);
