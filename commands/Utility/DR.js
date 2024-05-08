4a582917d0080b5d626304f765142ecff3530d3e
FabianXh <130483578+FabianXh@users.noreply.github.com>
2024-05-08 15:03:01 +0200

    DeathRoll fixes

--- commands/Utility/DR.js
+++ commands/Utility/DR.js
@@ -1,28 +1,69 @@
-const { SlashCommandBuilder, MessageEmbed } = require('@discordjs/builders');
+const {
+  Client,
+  GatewayIntentBits,
+  ButtonStyle,
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+} = require("discord.js");
+const { token } = require("../../config.json");
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const row = new ActionRowBuilder();
+const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
+
+row.addComponents(
+  new ButtonBuilder()
+    .setCustomId("rollMore")
+    .setLabel("Roll")
+    .setStyle(ButtonStyle.Danger)
+);
 
 const createEmbedMessage = (input, roll) =>
-    new MessageEmbed()
-        .setTitle('DEATH ROLL!')
-        .setDescription(`Roll a number between 1 and ${input}`)
-        .addFields({ name: 'ROLL', value: roll.toString() });
+  new EmbedBuilder()
+    .setColor("DC143C")
+    .setTitle("DEATH ROLL!")
+    .setDescription(`Rolling a number between 1 and ${input}...`)
+    .addFields({ name: "RESULT", value: roll.toString() });
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('death_roll')
-        .setDescription('DEATH ROLL!')
-        .addIntegerOption((option) =>
-            option
-                .setName('number')
-                .setDescription('Input the number to roll')
-                .setRequired(true)
-        ),
-    async execute(interaction) {
-        await interaction.deferReply();
-        const input = interaction.options.getInteger('number');
-        const roll = getRandomInt(input);
-        const embededMessageDR = createEmbedMessage(input, roll);
-        await interaction.editReply({ embeds: [embededMessageDR] });
-    },
+  data: new SlashCommandBuilder()
+    .setName("death_roll")
+    .setDescription("DEATH ROLL!")
+    .addIntegerOption((option) =>
+      option
+        .setName("number")
+        .setDescription("Input the number to roll")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+    const target = interaction?.options?.getUser("target");
+    const input = interaction.options.getInteger("number");
+    let roll = getRandomInt(input);
+    client.on("interactionCreate", (interaction) => {
+      if (!interaction.isButton() || interaction.customId !== "rollMore") {
+        return;
+      }
+
+      let oldMax = roll;
+      roll = getRandomInt(oldMax);
+
+      interaction.reply({
+        content: `${target}`,
+        embeds: [createEmbedMessage(oldMax, roll)],
+        components: [row],
+      });
+    });
+
+    await interaction.editReply({
+      content: `${target}`,
+      embeds: [createEmbedMessage(input, roll)],
+      components: [row],
+    });
+  },
 };
+
+client.login(token);
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Require the necessary discord.js classes
-const {
-    Client,
-    Collection,
-    GatewayIntentBits,
-    ActivityFlagsBitField,
-} = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 const { token } = require("./config.json");
@@ -18,37 +13,37 @@ const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter((file) => file.endsWith(".js"));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ("data" in command && "execute" in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(
-                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-            );
-        }
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
+  }
 }
 
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs
-    .readdirSync(eventsPath)
-    .filter((file) => file.endsWith(".js"));
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 client.login(token);
