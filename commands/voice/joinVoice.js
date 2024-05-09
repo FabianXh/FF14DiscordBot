45a406ea4b057a282ed605f6e6e6f7e8856ab32e
FabianXh <130483578+FabianXh@users.noreply.github.com>
2024-05-09 13:11:31 +0200

    Unused imports

--- commands/voice/joinVoice.js
+++ commands/voice/joinVoice.js
@@ -1,28 +1,26 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel } = require('@discordjs/voice');
-const { ChannelType, Client, GatewayIntentBits } = require('discord.js');
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { joinVoiceChannel } = require("@discordjs/voice");
+const { ChannelType } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('joinvc')
-        .setDescription('Join vc')
-        .addChannelOption((option) =>
-            option
-                .setName('channel')
-                .setDescription('VC channel to join')
-                .setRequired(true)
-                .addChannelTypes(ChannelType.GuildVoice)
-        ),
-    async execute(interaction) {
-        if (interaction.commandName === 'joinvc') {
-            const connect = joinVoiceChannel({
-                channelId: interaction.options.getChannel('channel').id,
-                guildId: interaction.guildId,
-                adapterCreator: interaction.guild.voiceAdapterCreator,
-            });
-        }
-        await interaction.reply('Wenz Pull! <se.6> (its working)');
-    },
+  data: new SlashCommandBuilder()
+    .setName("joinvc")
+    .setDescription("Join vc")
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("VC channel to join")
+        .setRequired(true)
+        .addChannelTypes(ChannelType.GuildVoice)
+    ),
+  async execute(interaction) {
+    if (interaction.commandName === "joinvc") {
+      const connect = joinVoiceChannel({
+        channelId: interaction.options.getChannel("channel").id,
+        guildId: interaction.guildId,
+        adapterCreator: interaction.guild.voiceAdapterCreator,
+      });
+    }
+    await interaction.reply("Wenz Pull! <se.6> (its working)");
+  },
 };
