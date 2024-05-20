036a6092cac994db3961cdc87bc8862b8aa4c58d
Matthew Nock <thematt.nock@gmail.com>
2024-05-20 19:58:00 +0100

    Poll stuff

--- commands/Utility/Poll.js
+++ commands/Utility/Poll.js
@@ -7,18 +7,72 @@ module.exports = {
     async execute(interaction) {
         await interaction.reply({
             poll: {
-                question: { text: 'some text here' },
-                answers: [{ text: 'Aye' }, { text: 'Nay' }],
+                question: { text: 'Saturday 13:00-15:00 ST' },
+                answers: [
+                    { text: 'p9s' },
+                    { text: 'p10s' },
+                    { text: 'p11s' },
+                    { text: 'p12s p1' },
+                    { text: 'p12s p2' },
+                ],
                 duration: 1,
-                allowMultiselect: false,
+                allowMultiselect: true,
             },
         });
         await interaction.followUp({
             poll: {
-                question: { text: 'some text here' },
-                answers: [{ text: 'Aye' }, { text: 'Nay' }],
+                question: { text: 'Saturday 15:00-17:00 ST' },
+                answers: [
+                    { text: 'p9s' },
+                    { text: 'p10s' },
+                    { text: 'p11s' },
+                    { text: 'p12s p1' },
+                    { text: 'p12s p2' },
+                ],
                 duration: 1,
-                allowMultiselect: false,
+                allowMultiselect: true,
+            },
+        });
+        await interaction.followUp({
+            poll: {
+                question: { text: 'Saturday 17:00-19:00 ST' },
+                answers: [
+                    { text: 'p9s' },
+                    { text: 'p10s' },
+                    { text: 'p11s' },
+                    { text: 'p12s p1' },
+                    { text: 'p12s p2' },
+                ],
+                duration: 1,
+                allowMultiselect: true,
+            },
+        });
+        await interaction.followUp({
+            poll: {
+                question: { text: 'Saturday 19:00-21:00 ST' },
+                answers: [
+                    { text: 'p9s' },
+                    { text: 'p10s' },
+                    { text: 'p11s' },
+                    { text: 'p12s p1' },
+                    { text: 'p12s p2' },
+                ],
+                duration: 1,
+                allowMultiselect: true,
+            },
+        });
+        await interaction.followUp({
+            poll: {
+                question: { text: 'Saturday 21:00-23:00 ST' },
+                answers: [
+                    { text: 'p9s' },
+                    { text: 'p10s' },
+                    { text: 'p11s' },
+                    { text: 'p12s p1' },
+                    { text: 'p12s p2' },
+                ],
+                duration: 1,
+                allowMultiselect: true,
             },
         });
     },
