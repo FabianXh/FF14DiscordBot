91d9a10c588108ff5334d1f13df1bbb71b0a1ac3
Matthew Nock <thematt.nock@gmail.com>
2024-05-20 21:40:34 +0100

    24 hour hendling

--- commands/Utility/Poll.js
+++ commands/Utility/Poll.js
@@ -4,21 +4,24 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('poll')
         .setDescription('poll testing')
-        .addIntegerOption((option) =>
+        .addStringOption((option) =>
             option
                 .setName('start_time')
                 .setDescription(
-                    'first poll start time (hour only) 13 would be 13:00 ST'
+                    'first poll start time in 24 hour format (e.g. 9 for 9:00 ST)'
                 )
                 .setRequired(true)
         ),
     async execute(interaction) {
-        let startTime = interaction.options.getInteger('start_time');
+        let startTime = interaction.options.getString('start_time');
+        startTime = parseInt(startTime.split(':')[0], 10);
+        let endTime = startTime + 2;
         console.log(startTime);
+
         await interaction.reply({
             poll: {
                 question: {
-                    text: `Saturday ${startTime}:00-${startTime + 2}:00 ST`,
+                    text: `Saturday ${startTime}:00-${endTime}:00 ST`,
                 },
                 answers: [
                     { text: 'p9s' },
@@ -31,10 +34,12 @@ module.exports = {
                 allowMultiselect: true,
             },
         });
+        startTime = (startTime + 2) % 24;
+        endTime = (startTime + 2) % 24;
         await interaction.followUp({
             poll: {
                 question: {
-                    text: `Saturday ${startTime + 2}:00-${startTime + 4}:00 ST`,
+                    text: `Saturday ${startTime}:00-${endTime}:00 ST`,
                 },
                 answers: [
                     { text: 'p9s' },
@@ -47,10 +52,12 @@ module.exports = {
                 allowMultiselect: true,
             },
         });
+        startTime = (startTime + 2) % 24;
+        endTime = (startTime + 2) % 24;
         await interaction.followUp({
             poll: {
                 question: {
-                    text: `Saturday ${startTime + 4}:00-${startTime + 6}:00 ST`,
+                    text: `Saturday ${startTime}:00-${endTime}:00 ST`,
                 },
                 answers: [
                     { text: 'p9s' },
@@ -63,10 +70,12 @@ module.exports = {
                 allowMultiselect: true,
             },
         });
+        startTime = (startTime + 2) % 24;
+        endTime = (startTime + 2) % 24;
         await interaction.followUp({
             poll: {
                 question: {
-                    text: `Saturday ${startTime + 6}:00-${startTime + 8}:00 ST`,
+                    text: `Saturday ${startTime}:00-${endTime}:00 ST`,
                 },
                 answers: [
                     { text: 'p9s' },
@@ -79,12 +88,12 @@ module.exports = {
                 allowMultiselect: true,
             },
         });
+        startTime = (startTime + 2) % 24;
+        endTime = (startTime + 2) % 24;
         await interaction.followUp({
             poll: {
                 question: {
-                    text: `Saturday ${startTime + 8}:00-${
-                        startTime + 10
-                    }:00 ST`,
+                    text: `Saturday ${startTime}:00-${endTime}:00 ST`,
                 },
                 answers: [
                     { text: 'p9s' },
