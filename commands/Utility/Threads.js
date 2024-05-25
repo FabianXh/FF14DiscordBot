73336f66189ea0567f5f4f970df7b63ea5532db9
Matthew Nock <thematt.nock@gmail.com>
2024-05-25 09:31:54 +0100

    need to look at message details.... finding polls within the message data?

--- commands/Utility/Threads.js
+++ commands/Utility/Threads.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { getMessages, returnMessages } = require('./Poll.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('threads')
@@ -7,6 +7,6 @@ module.exports = {
             'create a thread for each raid that got a cirtain number of votes'
         ),
     async execute(interaction) {
-        await interaction.reply('');
+        const messages = getMessages();
     },
 };
